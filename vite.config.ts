dc855222fe7cc79416618d15d670343ae71707e0
spiritEcosse <shevchenkcoigor@gmail.com>
2024-04-06 22:10:53 +0200

    Add Sentry Vite plugin to dependencies and config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "spiritecosse",
     project: "faithfishart-client"
+  }), sentryVitePlugin({
+    org: "spiritecosse",
+    project: "faithfishart-client"
   })],
 
   define: {
