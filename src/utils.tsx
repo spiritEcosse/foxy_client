2cdc79213246974dd40658efffd643a4753a5ed1
spiritEcosse <shevchenkcoigor@gmail.com>
2024-04-08 00:09:50 +0200

    Refactor fetchData to use async/await syntax

--- src/utils.tsx
+++ src/utils.tsx
@@ -10,32 +10,31 @@ class CustomError extends Error {
     }
 }
 
-export const fetchData = (path: string) => {
-    return axios.get(`${import.meta.env.VITE_APP_SERVER_URL}/api/v1/${path}`)
-        .then(response => {
-            const isJson = response.headers['content-type']?.includes('application/json') ?? false;
-            if (!isJson) {
-                throw new Error('Response is not JSON.');
-            }
-            return response.data;
-        })
-        .catch(error => {
-            let message = 'Unknown Error';
-            let code = 424;
+export const fetchData =  async (path: string) => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_APP_SERVER_URL}/api/v1/${path}`);
+        const isJson = response.headers['content-type']?.includes('application/json') ?? false;
+        if (!isJson) {
+            throw new Error('Response is not JSON.');
+        }
+        return response.data;
+    } catch (error) {
+        let message = 'Unknown Error';
+        let code = 424;
 
-            if (axios.isAxiosError(error)) {
-                const axiosError = error;
-                if (!axiosError.response) {
-                    message = 'No Server Response';
-                } else if (axiosError.code === 'ERR_NETWORK') {
-                    message = 'Network Error';
-                } else if (axiosError.response.status) {
-                    message = 'An error occurred';
-                    code = axiosError.response.status;
-                } else if (axiosError.code) {
-                    code = parseInt(axiosError.code, 10) ?? 424;
-                }
+        if (axios.isAxiosError(error)) {
+            const axiosError = error;
+            if (!axiosError.response) {
+                message = 'No Server Response';
+            } else if (axiosError.code === 'ERR_NETWORK') {
+                message = 'Network Error';
+            } else if (axiosError.response.status) {
+                message = 'An error occurred';
+                code = axiosError.response.status;
+            } else if (axiosError.code) {
+                code = parseInt(axiosError.code, 10) ?? 424;
             }
-            throw new CustomError( code, message );
-        });
+        }
+        throw new CustomError(code, message);
+    }
 };
